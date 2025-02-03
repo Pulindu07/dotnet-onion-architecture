3a7909d2f8b24d39d9954f08f4a0b36cdae22ea7
pulindu <pulindujanith@gmail.com>
2025-02-03 22:33:46 +0530

    test

--- src/MyApp.WebApi/Controllers/AllPhotosController.cs
+++ src/MyApp.WebApi/Controllers/AllPhotosController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Mvc;
 using MyApp.Application.Interfaces;
 using MyApp.Application.Models.Requests;
 using MyApp.Application.Models.Responses;
@@ -10,10 +11,12 @@ namespace MyApp.WebApi.Controllers
     public class AllPhotosController : ControllerBase
     {
         private readonly IPhotoService _photoService;
+        private readonly IConfiguration _configuration;
 
-        public AllPhotosController(IPhotoService photoService)
+        public AllPhotosController(IPhotoService photoService,IConfiguration configuration)
         {
             _photoService = photoService;
+            _configuration = configuration;
         }
 
         [HttpGet ("getPhotos")]
@@ -29,6 +32,21 @@ namespace MyApp.WebApi.Controllers
             var result = await _photoService.LikePhoto(request);
             return Ok(result);
         }
-        
+
+        [HttpGet("test-openai")]
+        public async Task<IActionResult> TestOpenAI()
+        {
+            var key = Environment.GetEnvironmentVariable("OPEN_AI_API_KEY")
+                            ?? _configuration["OpenAI:ApiKey"];
+
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", key);
+
+            var response = await httpClient.GetAsync("https://api.openai.com/v1/models");
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            return Ok(new { Status = response.StatusCode, Response = result });
+        }
     }
 }
